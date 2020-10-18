af30d2ee509a50f8c27930f7c618211d5b0b33ca
Mathilda Carlgren <Mathilda.Carlgren.1802@student.uu.se>
2020-10-18 22:03:07 +0200

    ändrat OrderForm, ej klar ännu

--- OOP2-Project-EA3/Forms/OrdersForm.Designer.cs
+++ OOP2-Project-EA3/Forms/OrdersForm.Designer.cs
@@ -28,12 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dispatchedOrdersListLB = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.processOrderBtn = new System.Windows.Forms.Button();
+            this.customerPhoneLabel = new System.Windows.Forms.Label();
+            this.customerPhoneTB = new System.Windows.Forms.TextBox();
+            this.customerEmailTB = new System.Windows.Forms.TextBox();
+            this.customerEmailLabel = new System.Windows.Forms.Label();
+            this.customerNameLabel = new System.Windows.Forms.Label();
+            this.customerNameTB = new System.Windows.Forms.TextBox();
+            this.dispatchedDateLabel = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pendingOrdersListLB = new System.Windows.Forms.ListBox();
+            this.dispatchedOrdersLabel = new System.Windows.Forms.Label();
+            this.pendingOrdersLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dispatchedOrdersListLB
+            // 
+            this.dispatchedOrdersListLB.FormattingEnabled = true;
+            this.dispatchedOrdersListLB.ItemHeight = 32;
+            this.dispatchedOrdersListLB.Location = new System.Drawing.Point(79, 72);
+            this.dispatchedOrdersListLB.Name = "dispatchedOrdersListLB";
+            this.dispatchedOrdersListLB.Size = new System.Drawing.Size(303, 676);
+            this.dispatchedOrdersListLB.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 702);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // processOrderBtn
+            // 
+            this.processOrderBtn.Location = new System.Drawing.Point(419, 479);
+            this.processOrderBtn.Name = "processOrderBtn";
+            this.processOrderBtn.Size = new System.Drawing.Size(200, 53);
+            this.processOrderBtn.TabIndex = 9;
+            this.processOrderBtn.Text = "Add";
+            this.processOrderBtn.UseVisualStyleBackColor = true;
+            // 
+            // customerPhoneLabel
+            // 
+            this.customerPhoneLabel.AutoSize = true;
+            this.customerPhoneLabel.Location = new System.Drawing.Point(419, 366);
+            this.customerPhoneLabel.Name = "customerPhoneLabel";
+            this.customerPhoneLabel.Size = new System.Drawing.Size(193, 32);
+            this.customerPhoneLabel.TabIndex = 8;
+            this.customerPhoneLabel.Text = "Customer phone";
+            // 
+            // customerPhoneTB
+            // 
+            this.customerPhoneTB.Location = new System.Drawing.Point(419, 401);
+            this.customerPhoneTB.Name = "customerPhoneTB";
+            this.customerPhoneTB.Size = new System.Drawing.Size(200, 39);
+            this.customerPhoneTB.TabIndex = 7;
+            // 
+            // customerEmailTB
+            // 
+            this.customerEmailTB.Location = new System.Drawing.Point(419, 314);
+            this.customerEmailTB.Name = "customerEmailTB";
+            this.customerEmailTB.Size = new System.Drawing.Size(200, 39);
+            this.customerEmailTB.TabIndex = 6;
+            // 
+            // customerEmailLabel
+            // 
+            this.customerEmailLabel.AutoSize = true;
+            this.customerEmailLabel.Location = new System.Drawing.Point(419, 279);
+            this.customerEmailLabel.Name = "customerEmailLabel";
+            this.customerEmailLabel.Size = new System.Drawing.Size(182, 32);
+            this.customerEmailLabel.TabIndex = 5;
+            this.customerEmailLabel.Text = "Customer email";
+            // 
+            // customerNameLabel
+            // 
+            this.customerNameLabel.AutoSize = true;
+            this.customerNameLabel.Location = new System.Drawing.Point(419, 184);
+            this.customerNameLabel.Name = "customerNameLabel";
+            this.customerNameLabel.Size = new System.Drawing.Size(184, 32);
+            this.customerNameLabel.TabIndex = 3;
+            this.customerNameLabel.Text = "Customer name";
+            // 
+            // customerNameTB
+            // 
+            this.customerNameTB.Location = new System.Drawing.Point(419, 220);
+            this.customerNameTB.Name = "customerNameTB";
+            this.customerNameTB.Size = new System.Drawing.Size(200, 39);
+            this.customerNameTB.TabIndex = 4;
+            // 
+            // dispatchedDateLabel
+            // 
+            this.dispatchedDateLabel.AutoSize = true;
+            this.dispatchedDateLabel.Location = new System.Drawing.Point(419, 95);
+            this.dispatchedDateLabel.Name = "dispatchedDateLabel";
+            this.dispatchedDateLabel.Size = new System.Drawing.Size(187, 32);
+            this.dispatchedDateLabel.TabIndex = 1;
+            this.dispatchedDateLabel.Text = "Dispatched date";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(419, 130);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(200, 39);
+            this.textBox5.TabIndex = 2;
+            // 
+            // pendingOrdersListLB
+            // 
+            this.pendingOrdersListLB.FormattingEnabled = true;
+            this.pendingOrdersListLB.ItemHeight = 32;
+            this.pendingOrdersListLB.Location = new System.Drawing.Point(907, 72);
+            this.pendingOrdersListLB.Name = "pendingOrdersListLB";
+            this.pendingOrdersListLB.Size = new System.Drawing.Size(303, 676);
+            this.pendingOrdersListLB.TabIndex = 0;
+            // 
+            // dispatchedOrdersLabel
+            // 
+            this.dispatchedOrdersLabel.AutoSize = true;
+            this.dispatchedOrdersLabel.Location = new System.Drawing.Point(79, 37);
+            this.dispatchedOrdersLabel.Name = "dispatchedOrdersLabel";
+            this.dispatchedOrdersLabel.Size = new System.Drawing.Size(207, 32);
+            this.dispatchedOrdersLabel.TabIndex = 15;
+            this.dispatchedOrdersLabel.Text = "Dispatched orders";
+            // 
+            // pendingOrdersLabel
+            // 
+            this.pendingOrdersLabel.AutoSize = true;
+            this.pendingOrdersLabel.Location = new System.Drawing.Point(907, 37);
+            this.pendingOrdersLabel.Name = "pendingOrdersLabel";
+            this.pendingOrdersLabel.Size = new System.Drawing.Size(175, 32);
+            this.pendingOrdersLabel.TabIndex = 16;
+            this.pendingOrdersLabel.Text = "Pending orders";
+            // 
+            // OrdersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1697, 1115);
+            this.Controls.Add(this.pendingOrdersLabel);
+            this.Controls.Add(this.dispatchedOrdersLabel);
+            this.Controls.Add(this.pendingOrdersListLB);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.dispatchedDateLabel);
+            this.Controls.Add(this.customerNameTB);
+            this.Controls.Add(this.customerNameLabel);
+            this.Controls.Add(this.customerEmailLabel);
+            this.Controls.Add(this.customerEmailTB);
+            this.Controls.Add(this.customerPhoneTB);
+            this.Controls.Add(this.customerPhoneLabel);
+            this.Controls.Add(this.processOrderBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dispatchedOrdersListLB);
+            this.Name = "OrdersForm";
             this.Text = "OrdersForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox dispatchedOrdersListLB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button processOrderBtn;
+        private System.Windows.Forms.Label customerPhoneLabel;
+        private System.Windows.Forms.TextBox customerPhoneTB;
+        private System.Windows.Forms.TextBox customerEmailTB;
+        private System.Windows.Forms.Label customerEmailLabel;
+        private System.Windows.Forms.Label customerNameLabel;
+        private System.Windows.Forms.TextBox customerNameTB;
+        private System.Windows.Forms.Label dispatchedDateLabel;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox pendingOrdersListLB;
+        private System.Windows.Forms.Label dispatchedOrdersLabel;
+        private System.Windows.Forms.Label pendingOrdersLabel;
     }
 }
