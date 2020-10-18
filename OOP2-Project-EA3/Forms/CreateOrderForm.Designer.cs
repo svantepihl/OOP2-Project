25e944bdb602ec66cbcd039d8fdc613638ada5c9
Mathilda Carlgren <Mathilda.Carlgren.1802@student.uu.se>
2020-10-18 10:35:15 +0200

    Fixat customerForm

--- OOP2-Project-EA3/Forms/CreateOrderForm.Designer.cs
+++ OOP2-Project-EA3/Forms/CreateOrderForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CreateOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1474, 973);
+            this.Name = "CreateOrderForm";
             this.Text = "CreateOrderForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
