80788b6ba95572e9a4dc752d1e322c1af90b7dc1
Svante Pihl <hej@svantepihl.com>
2020-10-19 13:35:56 +0200

    Kopplade ihop forms så man kan öppna från warehouseform

--- OOP2-Project-EA3/Forms/ProductForm.Designer.cs
+++ OOP2-Project-EA3/Forms/ProductForm.Designer.cs
@@ -49,140 +49,157 @@
             // productListLB
             // 
             this.productListLB.FormattingEnabled = true;
-            this.productListLB.ItemHeight = 32;
-            this.productListLB.Location = new System.Drawing.Point(43, 104);
+            this.productListLB.ItemHeight = 15;
+            this.productListLB.Location = new System.Drawing.Point(23, 49);
+            this.productListLB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productListLB.Name = "productListLB";
-            this.productListLB.Size = new System.Drawing.Size(303, 676);
+            this.productListLB.Size = new System.Drawing.Size(165, 319);
             this.productListLB.TabIndex = 0;
             // 
             // productCodeTB
             // 
-            this.productCodeTB.Location = new System.Drawing.Point(383, 162);
+            this.productCodeTB.Location = new System.Drawing.Point(206, 76);
+            this.productCodeTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productCodeTB.Name = "productCodeTB";
-            this.productCodeTB.Size = new System.Drawing.Size(200, 39);
+            this.productCodeTB.Size = new System.Drawing.Size(110, 23);
             this.productCodeTB.TabIndex = 2;
             // 
             // productCodeLabel
             // 
             this.productCodeLabel.AutoSize = true;
-            this.productCodeLabel.Location = new System.Drawing.Point(383, 127);
+            this.productCodeLabel.Location = new System.Drawing.Point(206, 60);
+            this.productCodeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productCodeLabel.Name = "productCodeLabel";
-            this.productCodeLabel.Size = new System.Drawing.Size(70, 32);
+            this.productCodeLabel.Size = new System.Drawing.Size(35, 15);
             this.productCodeLabel.TabIndex = 1;
             this.productCodeLabel.Text = "Code";
             // 
             // productNameTB
             // 
-            this.productNameTB.Location = new System.Drawing.Point(383, 252);
+            this.productNameTB.Location = new System.Drawing.Point(206, 118);
+            this.productNameTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productNameTB.Name = "productNameTB";
-            this.productNameTB.Size = new System.Drawing.Size(200, 39);
+            this.productNameTB.Size = new System.Drawing.Size(110, 23);
             this.productNameTB.TabIndex = 4;
             // 
             // productNameLabel
             // 
             this.productNameLabel.AutoSize = true;
-            this.productNameLabel.Location = new System.Drawing.Point(383, 216);
+            this.productNameLabel.Location = new System.Drawing.Point(206, 101);
+            this.productNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productNameLabel.Name = "productNameLabel";
-            this.productNameLabel.Size = new System.Drawing.Size(78, 32);
+            this.productNameLabel.Size = new System.Drawing.Size(39, 15);
             this.productNameLabel.TabIndex = 3;
             this.productNameLabel.Text = "Name";
             // 
             // productPriceLabel
             // 
             this.productPriceLabel.AutoSize = true;
-            this.productPriceLabel.Location = new System.Drawing.Point(383, 311);
+            this.productPriceLabel.Location = new System.Drawing.Point(206, 146);
+            this.productPriceLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productPriceLabel.Name = "productPriceLabel";
-            this.productPriceLabel.Size = new System.Drawing.Size(65, 32);
+            this.productPriceLabel.Size = new System.Drawing.Size(33, 15);
             this.productPriceLabel.TabIndex = 5;
             this.productPriceLabel.Text = "Price";
             // 
             // productPriceTB
             // 
-            this.productPriceTB.Location = new System.Drawing.Point(383, 346);
+            this.productPriceTB.Location = new System.Drawing.Point(206, 162);
+            this.productPriceTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productPriceTB.Name = "productPriceTB";
-            this.productPriceTB.Size = new System.Drawing.Size(200, 39);
+            this.productPriceTB.Size = new System.Drawing.Size(110, 23);
             this.productPriceTB.TabIndex = 6;
             // 
             // productStockTB
             // 
-            this.productStockTB.Location = new System.Drawing.Point(383, 433);
+            this.productStockTB.Location = new System.Drawing.Point(206, 203);
+            this.productStockTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productStockTB.Name = "productStockTB";
-            this.productStockTB.Size = new System.Drawing.Size(200, 39);
+            this.productStockTB.Size = new System.Drawing.Size(110, 23);
             this.productStockTB.TabIndex = 7;
             // 
             // productStockLabel
             // 
             this.productStockLabel.AutoSize = true;
-            this.productStockLabel.Location = new System.Drawing.Point(383, 398);
+            this.productStockLabel.Location = new System.Drawing.Point(206, 187);
+            this.productStockLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productStockLabel.Name = "productStockLabel";
-            this.productStockLabel.Size = new System.Drawing.Size(71, 32);
+            this.productStockLabel.Size = new System.Drawing.Size(36, 15);
             this.productStockLabel.TabIndex = 8;
             this.productStockLabel.Text = "Stock";
             // 
             // updateProductBtn
             // 
-            this.updateProductBtn.Location = new System.Drawing.Point(382, 680);
+            this.updateProductBtn.Location = new System.Drawing.Point(206, 319);
+            this.updateProductBtn.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.updateProductBtn.Name = "updateProductBtn";
-            this.updateProductBtn.Size = new System.Drawing.Size(201, 46);
+            this.updateProductBtn.Size = new System.Drawing.Size(108, 22);
             this.updateProductBtn.TabIndex = 10;
             this.updateProductBtn.Text = "Update";
             this.updateProductBtn.UseVisualStyleBackColor = true;
             // 
             // productAvailableTB
             // 
-            this.productAvailableTB.Location = new System.Drawing.Point(383, 527);
+            this.productAvailableTB.Location = new System.Drawing.Point(206, 247);
+            this.productAvailableTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productAvailableTB.Name = "productAvailableTB";
-            this.productAvailableTB.Size = new System.Drawing.Size(200, 39);
+            this.productAvailableTB.Size = new System.Drawing.Size(110, 23);
             this.productAvailableTB.TabIndex = 11;
             // 
             // productAvailableLabel
             // 
             this.productAvailableLabel.AutoSize = true;
-            this.productAvailableLabel.Location = new System.Drawing.Point(383, 492);
+            this.productAvailableLabel.Location = new System.Drawing.Point(206, 231);
+            this.productAvailableLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productAvailableLabel.Name = "productAvailableLabel";
-            this.productAvailableLabel.Size = new System.Drawing.Size(158, 32);
+            this.productAvailableLabel.Size = new System.Drawing.Size(78, 15);
             this.productAvailableLabel.TabIndex = 12;
             this.productAvailableLabel.Text = "First available";
             // 
             // productStockingLabel
             // 
             this.productStockingLabel.AutoSize = true;
-            this.productStockingLabel.Location = new System.Drawing.Point(383, 585);
+            this.productStockingLabel.Location = new System.Drawing.Point(206, 274);
+            this.productStockingLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.productStockingLabel.Name = "productStockingLabel";
-            this.productStockingLabel.Size = new System.Drawing.Size(160, 32);
+            this.productStockingLabel.Size = new System.Drawing.Size(80, 15);
             this.productStockingLabel.TabIndex = 13;
             this.productStockingLabel.Text = "Next stocking";
             // 
             // productStockingTB
             // 
-            this.productStockingTB.Location = new System.Drawing.Point(383, 621);
+            this.productStockingTB.Location = new System.Drawing.Point(206, 291);
+            this.productStockingTB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productStockingTB.Name = "productStockingTB";
-            this.productStockingTB.Size = new System.Drawing.Size(200, 39);
+            this.productStockingTB.Size = new System.Drawing.Size(110, 23);
             this.productStockingTB.TabIndex = 14;
             // 
             // productStockZeroListLB
             // 
             this.productStockZeroListLB.FormattingEnabled = true;
-            this.productStockZeroListLB.ItemHeight = 32;
-            this.productStockZeroListLB.Location = new System.Drawing.Point(961, 104);
+            this.productStockZeroListLB.ItemHeight = 15;
+            this.productStockZeroListLB.Location = new System.Drawing.Point(517, 49);
+            this.productStockZeroListLB.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.productStockZeroListLB.Name = "productStockZeroListLB";
-            this.productStockZeroListLB.Size = new System.Drawing.Size(420, 676);
+            this.productStockZeroListLB.Size = new System.Drawing.Size(228, 319);
             this.productStockZeroListLB.TabIndex = 15;
             // 
             // createNewProductBtn
             // 
-            this.createNewProductBtn.Location = new System.Drawing.Point(43, 805);
+            this.createNewProductBtn.Location = new System.Drawing.Point(23, 377);
+            this.createNewProductBtn.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.createNewProductBtn.Name = "createNewProductBtn";
-            this.createNewProductBtn.Size = new System.Drawing.Size(303, 78);
+            this.createNewProductBtn.Size = new System.Drawing.Size(163, 37);
             this.createNewProductBtn.TabIndex = 9;
             this.createNewProductBtn.Text = "Create new product";
             this.createNewProductBtn.UseVisualStyleBackColor = true;
+            this.createNewProductBtn.Click += new System.EventHandler(this.createNewProductBtn_Click);
             // 
             // ProductForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1565, 1121);
+            this.ClientSize = new System.Drawing.Size(843, 525);
             this.Controls.Add(this.createNewProductBtn);
             this.Controls.Add(this.productStockZeroListLB);
             this.Controls.Add(this.productStockingTB);
@@ -199,6 +216,7 @@
             this.Controls.Add(this.productCodeLabel);
             this.Controls.Add(this.productCodeTB);
             this.Controls.Add(this.productListLB);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "ProductForm";
             this.Text = "ProductForm";
             this.ResumeLayout(false);
